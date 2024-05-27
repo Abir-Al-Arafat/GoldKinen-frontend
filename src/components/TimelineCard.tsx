0645e04fc82195d0f0563c81e8a108614a4e2de1
Abir-Al-Arafat <antorabir@gmail.com>
2024-05-27 17:25:13 +0600

    component updated for rendering comments

--- src/components/TimelineCard.tsx
+++ src/components/TimelineCard.tsx
@@ -1,12 +1,28 @@
 import Card from "react-bootstrap/Card";
+import useComments from "../react-query/hooks/useComments";
+import { Comment } from "../react-query/hooks/useComments";
+import { useState } from "react";
+import { Button, ListGroup } from "react-bootstrap";
 
 interface TimelineCardProps {
   title: string;
   description: string;
   username: string;
+  postId: number;
 }
 
-function TimelineCard({ title, description, username }: TimelineCardProps) {
+function TimelineCard({
+  title,
+  description,
+  username,
+  postId,
+}: TimelineCardProps) {
+  const [showComments, setShowComments] = useState(false);
+  const { data: comments, isLoading, error } = useComments(postId);
+
+  const handleToggleComments = () => {
+    setShowComments(!showComments);
+  };
   return (
     <Card style={{ width: "28rem" }}>
       <Card.Body>
@@ -18,6 +34,28 @@ function TimelineCard({ title, description, username }: TimelineCardProps) {
           {" "}
           <span className="fw-medium"> Description:</span> {description}
         </Card.Text>
+
+        <Button onClick={handleToggleComments} variant="link">
+          {showComments ? "Hide Comments" : "Show Comments"}
+        </Button>
+        {showComments && (
+          <>
+            {isLoading ? (
+              <p>Loading comments...</p>
+            ) : error ? (
+              <p>{error.message}</p>
+            ) : (
+              <ListGroup variant="flush">
+                {comments?.map((comment: Comment) => (
+                  <ListGroup.Item key={comment.id}>
+                    <strong>{comment.name}</strong> <br />
+                    {comment.body}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            )}
+          </>
+        )}
       </Card.Body>
     </Card>
   );
